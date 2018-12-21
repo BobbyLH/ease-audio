3884674252a50394933d8d859a00477cf31d9ff7
bobby.li <bobby.li@ximalaya.com>
2018-12-21 18:21:42 +0800

    auto build

---
+++ build/git.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const pkg = fs.readFileSync('./package.json', 'utf-8')
+const match = pkg.match(/version.*(\d+).(\d+).(\d+)/)
+const version = `${match[1]}.${match[2]}.${match[3]}`
+
+async function autoGit () {
+  const simpleGit = require('simple-git/promise')
+  const git = simpleGit(__dirname)
+  const {current} = await git.branch()
+  const branch = (process.argv && process.argv[2]) || current
+  const message = `version - ${version}`
+  let gitStatus = null
+  try {
+    gitStatus = await git.add('../*')
+    gitStatus = await git.commit(`[Ease Audio]: ${message}`)
+    gitStatus = await git.push('origin', branch)
+    console.log(`[git - status]: ${gitStatus}`)
+  } catch (e) {
+    // handle the error
+    console.log(`[git - error]: ${e}`)
+    return true
+  }
+  return true
+}
+
+autoGit()
