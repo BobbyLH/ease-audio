2d9eefaf78eb1827fd38637cdc57975d8d392358
bobby.li <bobby.li@ximalaya.com>
2019-01-07 20:14:08 +0800

    [Ease Audio]: version - 0.0.39

--- dist/ease-audio.js
+++ dist/ease-audio.js
@@ -255,12 +255,36 @@
     }, {
       key: "play",
       value: function play() {
-        if (this._checkInit()) {
-          this._blockEvent({
-            block: false
-          });
+        var _this = this;
 
-          this.audioH5.play();
+        if (this._checkInit()) {
+          try {
+            this._blockEvent({
+              block: false
+            });
+
+            var play = this.audioH5.play();
+
+            if (play && typeof Promise !== 'undefined' && (play instanceof Promise || typeof play.then === 'function')) {
+              play.catch(function (err) {
+                _this._setPlayState(playStateSet[6]);
+
+                _this._fireEventQueue(err, 'onplayerror');
+              });
+            } // If the sound is still paused, then we can assume there was a playback issue.
+
+
+            if (this.audioH5.paused) {
+              this._setPlayState(playStateSet[6]);
+
+              this._fireEventQueue(this.playId, 'onplayerror');
+            }
+          } catch (err) {
+            this._setPlayState(playStateSet[6]);
+
+            this._fireEventQueue(err, 'onplayerror');
+          }
+
           return this.playId;
         }
       }
@@ -468,7 +492,7 @@
     }, {
       key: "once",
       value: function once(event, cb) {
-        var _this = this;
+        var _this2 = this;
 
         if (this._checkType(event, 'string', true) && this._checkType(cb, 'function', true)) {
           var queueName = event.indexOf('on') === 0 ? event : "on".concat(event);
@@ -477,7 +501,7 @@
           var once = function once(e) {
             cb && cb(e);
 
-            _this._offEvent(queueName, once, funcName);
+            _this2._offEvent(queueName, once, funcName);
           };
 
           this._onEvent(queueName, once, funcName);
@@ -677,7 +701,7 @@
     }, {
       key: "_updatePlayList",
       value: function _updatePlayList(_ref2) {
-        var _this2 = this;
+        var _this3 = this;
 
         var action = _ref2.action,
             list = _ref2.list,
@@ -686,9 +710,9 @@
         switch (action) {
           case 'add':
             this.playList = [].concat(_toConsumableArray(this.playList), _toConsumableArray(list.map(function (v) {
-              if (_this2._checkType(v, 'object')) {
-                v.playId = _this2.idCounter;
-                _this2.idCounter++;
+              if (_this3._checkType(v, 'object')) {
+                v.playId = _this3.idCounter;
+                _this3.idCounter++;
                 return v;
               }
             })));
@@ -801,11 +825,11 @@
     }, {
       key: "_registerEvent",
       value: function _registerEvent(config) {
-        var _this3 = this;
+        var _this4 = this;
 
         var curry = function curry(cb, eventName) {
           return function (e) {
-            if (!_this3._triggerEventController(eventName)) return;
+            if (!_this4._triggerEventController(eventName)) return;
             return cb && cb(e);
           };
         };
@@ -817,54 +841,48 @@
           if (v.indexOf('on') === 0) {
             var funcName = "EASE_AUDIO_".concat(v.toUpperCase(), "_INITIAL_CALLBACK");
 
-            _this3._onEvent(v, config[v], funcName);
+            _this4._onEvent(v, config[v], funcName);
           }
         });
         this.eventMethods = {
           // loading state
           loadstart: function loadstart(e) {
-            _this3.playState === playStateSet[1] && _this3._setPlayState(playStateSet[0]);
+            _this4.playState === playStateSet[1] && _this4._setPlayState(playStateSet[0]);
 
-            _this3._fireEventQueue(e, 'onload');
+            _this4._fireEventQueue(e, 'onload');
           },
           // playing state
           playing: function playing(e) {
-            _this3._setPlayState(playStateSet[1]);
+            _this4._setPlayState(playStateSet[1]);
 
-            _this3._fireEventQueue(e, 'onplay');
+            _this4._fireEventQueue(e, 'onplay');
           },
           canplaythrough: function canplaythrough(e) {
-            _this3.playState === playStateSet[0] && _this3._setPlayState(playStateSet[1]);
+            _this4.playState === playStateSet[0] && _this4._setPlayState(playStateSet[1]);
           },
           // paused state
           pause: function pause(e) {
-            _this3._setPlayState(playStateSet[2]);
+            _this4._setPlayState(playStateSet[2]);
 
-            _this3._fireEventQueue(e, 'onpause');
+            _this4._fireEventQueue(e, 'onpause');
           },
           // ended state
           ended: function ended(e) {
-            if (_this3.isEnd) {
-              _this3.isEnd = false;
+            if (_this4.isEnd) {
+              _this4.isEnd = false;
             } else {
-              _this3.isEnd = true;
+              _this4.isEnd = true;
 
-              _this3._fireEventQueue(e, 'onend');
+              _this4._fireEventQueue(e, 'onend');
 
-              _this3.config.endAutoCut && _this3._cut(true);
+              _this4.config.endAutoCut && _this4._cut(true);
             }
           },
           // loaderror state
           error: function error(e) {
-            _this3._setPlayState(playStateSet[5]);
+            _this4._setPlayState(playStateSet[5]);
 
-            _this3._fireEventQueue(e, 'onloaderror');
-          },
-          // playerror state
-          stalled: function stalled(e) {
-            _this3._setPlayState(playStateSet[6]);
-
-            _this3._fireEventQueue(e, 'onplayerror');
+            _this4._fireEventQueue(e, 'onloaderror');
           },
           // others
           progress: function progress(e) {
@@ -876,7 +894,7 @@
 
             if (ranges && ranges.length) {
               for (var i = 0, j = ranges.length; i < j; i++) {
-                _this3.buffered.push({
+                _this4.buffered.push({
                   'start': ranges.start(i) * 1000,
                   'end': ranges.end(i) * 1000
                 });
@@ -887,59 +905,59 @@
               progress = loaded / total;
             }
 
-            _this3._fireEventQueue({
+            _this4._fireEventQueue({
               e: e,
               progress: progress
             }, 'onprogress');
           },
           durationchange: function durationchange(e) {},
           loadedmetadata: function loadedmetadata(e) {
-            _this3.metaDataLoaded = true;
-            _this3.seekValue && _this3.seek(_this3.seekValue);
+            _this4.metaDataLoaded = true;
+            _this4.seekValue && _this4.seek(_this4.seekValue);
           },
           loadeddata: function loadeddata(e) {},
           timeupdate: function timeupdate(e) {
             // playState is loading but actually is playing
-            if (_this3.playState === playStateSet[0]) {
-              _this3._logInfo("timeupdate's playing");
+            if (_this4.playState === playStateSet[0]) {
+              _this4._logInfo("timeupdate's playing");
 
-              _this3._setPlayState(playStateSet[1]);
+              _this4._setPlayState(playStateSet[1]);
 
-              _this3._fireEventQueue(e, 'onplay');
+              _this4._fireEventQueue(e, 'onplay');
             } // Depending on currentTime and duration to mimic end event
 
 
-            var isEnd = _this3.audioH5.duration && _this3.audioH5.currentTime === _this3.audioH5.duration;
+            var isEnd = _this4.audioH5.duration && _this4.audioH5.currentTime === _this4.audioH5.duration;
 
             if (isEnd) {
-              _this3._logInfo("timeupdate's ended");
+              _this4._logInfo("timeupdate's ended");
 
-              if (_this3.isEnd) {
-                _this3.isEnd = false;
+              if (_this4.isEnd) {
+                _this4.isEnd = false;
               } else {
-                _this3.isEnd = true;
+                _this4.isEnd = true;
 
-                _this3._fireEventQueue(e, 'onend');
+                _this4._fireEventQueue(e, 'onend');
 
-                _this3.config.endAutoCut && _this3._cut(true);
+                _this4.config.endAutoCut && _this4._cut(true);
               }
             }
 
-            _this3._fireEventQueue(e, 'ontimeupdate');
+            _this4._fireEventQueue(e, 'ontimeupdate');
           },
           canplay: function canplay(e) {
-            _this3._fireEventQueue(e, 'oncanplay');
+            _this4._fireEventQueue(e, 'oncanplay');
           },
           seeking: function seeking(e) {
-            _this3._fireEventQueue(e, 'onseek');
+            _this4._fireEventQueue(e, 'onseek');
           },
           seeked: function seeked(e) {},
           play: function play(e) {},
           volumechange: function volumechange(e) {
-            _this3._fireEventQueue(e, 'onvolume');
+            _this4._fireEventQueue(e, 'onvolume');
           },
           ratechange: function ratechange(e) {
-            _this3._fireEventQueue(e, 'onrate');
+            _this4._fireEventQueue(e, 'onrate');
           },
           abort: function abort(e) {},
           suspend: function suspend(e) {}
