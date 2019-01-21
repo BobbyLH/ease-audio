1122e253f074d54f96f6d3fe0c35c38e2f4414fe
bobby.li <bobby.li@ximalaya.com>
2019-01-21 15:21:59 +0800

    add necessary plugins for babel-transform-runtime

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,35 @@
-const presets = [[
-  '@babel/preset-env',
-  {
-    useBuiltIns: 'usage'
-  }
-]]
+const presets = []
 
-const plugins = []
+const plugins = [
+  [
+    '@babel/plugin-transform-classes'
+  ], [
+    '@babel/plugin-transform-destructuring'
+  ], [
+    '@babel/plugin-transform-arrow-functions'
+  ], [
+    '@babel/plugin-transform-function-name'
+  ], [
+    '@babel/plugin-transform-template-literals'
+  ], [
+    '@babel/plugin-transform-spread'
+  ], [
+    '@babel/plugin-proposal-object-rest-spread'
+  ], [
+    '@babel/plugin-transform-shorthand-properties'
+  ], [
+    '@babel/plugin-transform-parameters'
+  ], [
+    '@babel/plugin-transform-instanceof'
+  ], [
+    '@babel/plugin-transform-runtime',
+    {
+      'corejs': false,
+      'helpers': true,
+      'regenerator': true,
+      'useESModules': false
+    }
+  ]
+]
 
 module.exports = { presets, plugins }
