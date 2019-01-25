cad89ac2c07cf5c9e7e394d644c558edd6996ba6
bobby.li <bobby.li@ximalaya.com>
2019-01-25 17:23:19 +0800

    [Ease Audio]: version - 0.0.77

--- dist/ease-audio.js
+++ dist/ease-audio.js
@@ -599,8 +599,6 @@
       if (IteratorPrototype !== Object.prototype && IteratorPrototype.next) {
         // Set @@toStringTag to native iterators
         _setToStringTag(IteratorPrototype, TAG, true);
-        // fix for some old engines
-        if (!_library && typeof IteratorPrototype[ITERATOR] != 'function') _hide(IteratorPrototype, ITERATOR, returnThis);
       }
     }
     // fix Array#{values, @@iterator}.name in V8 / FF
@@ -609,7 +607,7 @@
       $default = function values() { return $native.call(this); };
     }
     // Define iterator
-    if ((!_library || FORCED) && (BUGGY || VALUES_BUG || !proto[ITERATOR])) {
+    if ((FORCED) && (BUGGY || VALUES_BUG || !proto[ITERATOR])) {
       _hide(proto, ITERATOR, $default);
     }
     // Plug for library
@@ -3023,7 +3021,7 @@
       key: "_log",
       value: function _log(detail) {
         var canLog = this.logLevel !== 'silent' && this.logLevel === 'detail';
-        return canLog && this.debug && console.log('[EASE_AUDIO_H5 DETAIL]:', detail);
+        return canLog && this.debug && this._logOptimize(detail, 'log');
       }
       /* info logger */
 
@@ -3031,7 +3029,7 @@
       key: "_logInfo",
       value: function _logInfo(info) {
         var canLog = this.logLevel !== 'silent' && this.logLevel !== 'error' && this.logLevel !== 'warn';
-        return canLog && this.debug && console.info('[EASE_AUDIO_H5 INFO]:', info);
+        return canLog && this.debug && this._logOptimize(info, 'info');
       }
       /* warn logger */
 
@@ -3039,15 +3037,30 @@
       key: "_logWarn",
       value: function _logWarn(warn) {
         var canLog = this.logLevel !== 'silent' && this.logLevel !== 'error';
-        return canLog && this.debug && console.warn('[EASE_AUDIO_H5 WARN]:', warn);
+        return canLog && this.debug && this._logOptimize(warn, 'warn');
       }
       /* error logger */
 
     }, {
       key: "_logErr",
-      value: function _logErr(err) {
+      value: function _logErr(error) {
         var canLog = this.logLevel !== 'silent';
-        return canLog && this.debug && console.error('[EASE_AUDIO_H5 ERROR]:', err);
+        return canLog && this.debug && this._logOptimize(error, 'error');
+      }
+      /* console log optimize */
+
+    }, {
+      key: "_logOptimize",
+      value: function _logOptimize(msg, method) {
+        var logger = console[method] || console.log;
+        var prefix = "[EASE_AUDIO_H5 ".concat(method.toUpperCase(), "]:");
+
+        if ((this._checkType(msg, 'object') || this._checkType(msg, 'array')) && console.table) {
+          logger(prefix);
+          return console.table(msg);
+        }
+
+        return logger(prefix, msg);
       }
     }, {
       key: "duration",
