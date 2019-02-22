06c30a4d8ea65cb70d2a56ff16fbb628c39fedbf
bobby.li <bobby.li@ximalaya.com>
2019-02-22 19:09:17 +0800

    fix bug

--- src/audio_h5.js
+++ src/audio_h5.js
@@ -142,7 +142,7 @@ export class AudioH5 {
   toggle () {
     if (this._checkInit() && this.playState !== playStateSet[6] && this.playState !== playStateSet[7] && this.playState !== playStateSet[8]) {
       if (this.waitPause) {
-        this.cancalPause = true
+        this.cancalPause = !this.cancalPause
       } else {
         if (this.playState === null || this.playState === 'paused') {
           // trigger play method
