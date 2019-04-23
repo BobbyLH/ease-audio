837dc0ea3d4862374350c7f4f64045f9fea9c096
bobby.li <bobby.li@ximalaya.com>
2019-04-23 16:06:24 +0800

    add test files

---
+++ test/index.js
@@ -0,0 +1,34 @@
+import EaseAudio from '../dist/ease-audio'
+// import 'mocha'
+// import { expect } from 'chai'
+
+console.log(EaseAudio, 222)
+// describe("EaseAudio's test module", function () {
+//   const audio = new EaseAudio()
+//   it('audio is an object', function () {
+//     expect(audio).to.be.an('object')
+//     expect(audio).to.have.property('audio').to.be.an('object')
+//   })
+//   it('audio - init', function () {
+//     const config = {
+//       debug: true,
+//       logLevel: 'info',
+//       // autoplay: true,
+//       // src: 'http://audio.xmcdn.com/group29/M01/AA/71/wKgJXVrpaoXApbrYABINQqa4hlE219.m4a',
+//       onplay: e => console.log('onplay:', e),
+//       onpause: e => console.log('onpause:', e),
+//       onstop: id => console.log('onstop', id),
+//       onseek: e => console.log('onseek:', e),
+//       onseeking: e => console.log('onseeking:', e),
+//       onprogress: e => console.log('onprogress:', e),
+//       playlist: [
+//         {src: 'http://audio.xmcdn.com/group29/M01/AA/71/wKgJXVrpaoXApbrYABINQqa4hlE219.m4a'},
+//         'http://audio.xmcdn.com/group36/M0A/28/2C/wKgJUloyLSPzMzrUAA_CiRLIGrE559.m4a',
+//         {src: 'http://audio.xmcdn.com/group21/M0B/2E/08/wKgJLVrpYaLCVIMPABFX6j5WjMk013.m4a'}
+//       ]
+//     }
+//     const initRes = audio.init(config)
+//     expect(initRes).to.be.an('object')
+//     expect(initRes).to.have.property('playlists')
+//   })
+// })
